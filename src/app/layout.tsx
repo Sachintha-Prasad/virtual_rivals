0805145ba13081ac8a9c9b3837e59316dd9f1b5b
Sachintha-Prasad <prasadsachintha1231@gmail.com>
2025-08-17 01:20:18 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,19 @@
-import type { Metadata } from "next"
-import { Chakra_Petch } from "next/font/google"
-import "./globals.css"
+import type { Metadata } from 'next'
+import { Chakra_Petch } from 'next/font/google'
+import './globals.css'
 
 const chakraPetch = Chakra_Petch({
-    weight: ["300", "400", "500", "600", "700"]
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700'],
 })
 
 export const metadata: Metadata = {
-    title: "Virtual Rivals",
-    description: "Virtual rivals definitive edition"
+    title: 'Virtual Rivals',
+    description: 'Virtual rivals definitive edition',
 }
 
 export default function RootLayout({
-    children
+    children,
 }: Readonly<{ children: React.ReactNode }>) {
     return (
         <html lang="en">
